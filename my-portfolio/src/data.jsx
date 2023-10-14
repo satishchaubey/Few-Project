ac9219b029ac99d463375f9210597296fd98ca92
satishchaubey <satishchaubey02@gmail.com>
2023-10-15 01:58:45 +0530

    test

---
+++ my-portfolio/src/data.jsx
@@ -0,0 +1,480 @@
+import {
+  FaHome,
+  FaUser,
+  FaFolderOpen,
+  FaEnvelopeOpen,
+  FaBriefcase,
+  FaGraduationCap,
+  FaCode,
+} from 'react-icons/fa';
+import { FiFileText, FiUser, FiExternalLink } from 'react-icons/fi';
+
+import Work1 from './assests/project-1.jpeg';
+import Work2 from './assests/project-2.jpg';
+import Work3 from './assests/project-3.jpeg';
+import Work4 from './assests/project-4.jpeg';
+import Work5 from './assests/project-5.jpg';
+import Work6 from './assests/project-6.jpg';
+
+import Theme1 from './assests/purple.png';
+import Theme2 from './assests/red.png';
+import Theme3 from './assests/blueviolet.png';
+import Theme4 from './assests/blue.png';
+import Theme5 from './assests/goldenrod.png';
+import Theme6 from './assests/magenta.png';
+import Theme7 from './assests/yellowgreen.png';
+import Theme8 from './assests/orange.png';
+import Theme9 from './assests/green.png';
+import Theme10 from './assests/yellow.png';
+
+export const links = [
+  {
+    id: 1,
+    name: 'Home',
+    icon: <FaHome className='nav__icon' />,
+    path: '/',
+  },
+
+  {
+    id: 2,
+    name: 'About',
+    icon: <FaUser className='nav__icon' />,
+    path: '/about',
+  },
+
+  {
+    id: 3,
+    name: 'Portfolio',
+    icon: <FaFolderOpen className='nav__icon' />,
+    path: '/portfolio',
+  },
+
+  {
+    id: 4,
+    name: 'Contact',
+    icon: <FaEnvelopeOpen className='nav__icon' />,
+    path: '/contact',
+  },
+];
+
+export const personalInfo = [
+  {
+    id: 1,
+    title: 'First Name : ',
+    description: 'Satish',
+  },
+
+  {
+    id: 2,
+    title: 'Last Name : ',
+    description: 'Chaubey',
+  },
+
+  {
+    id: 3,
+    title: 'Age : ',
+    description: '26 Years',
+  },
+
+  {
+    id: 4,
+    title: 'Nationality : ',
+    description: 'Indian',
+  },
+
+  {
+    id: 5,
+    title: 'Freelance : ',
+    description: 'Available',
+  },
+
+  {
+    id: 6,
+    title: 'Address : ',
+    description: 'Kushinagar',
+  },
+
+  {
+    id: 7,
+    title: 'Phone : ',
+    description: '8299805407',
+  },
+
+  {
+    id: 8,
+    title: 'Email : ',
+    description: 'satishchaubey02@gmail.com',
+  },
+
+  {
+    id: 9,
+    title: 'Skype : ',
+    description: 'Satish Kumar Chaubey',
+  },
+
+  {
+    id: 10,
+    title: 'Langages : ',
+    description: ' English ,Hindi ,Bhojpuri',
+  },
+];
+
+export const stats = [
+  {
+    id: 1,
+    no: '6',
+    title: 'Months of <br /> Experience',
+  },
+
+  {
+    id: 2,
+    no: '8',
+    title: 'Completed <br /> Projects',
+  },
+
+  {
+    id: 3,
+    no: '1',
+    title: 'Happy <br /> Customers',
+  },
+
+  {
+    id: 4,
+    no: '2',
+    title: ' Awards <br /> Won',
+  },
+];
+
+export const resume = [
+  {
+    id: 1,
+    category: 'experience',
+    icon: <FaBriefcase />,
+    year: '2022- PRESENT',
+    title: 'MERN STACK <span> Developer </span>',
+    desc: 'Lorem ipsum dolor sit amet, consectetur tempor incididunt ut labore adipisicing elit,',
+  },
+
+  {
+    id: 2,
+    category: 'experience',
+    icon: <FaBriefcase />,
+    year: '2013 - 2018',
+    title: 'UI/UX Designer <span> Themeforest </span>',
+    desc: 'Lorem incididunt dolor sit amet, consectetur eiusmod dunt doldunt dol elit, tempor incididunt',
+  },
+
+  {
+    id: 3,
+    category: 'experience',
+    icon: <FaBriefcase />,
+    year: '2005 - 2013',
+    title: 'Consultant <span> Videohive </span>',
+    desc: 'Lorem ipsum dolor sit amet, tempor incididunt ut laboreconsectetur elit, sed do eiusmod tempor duntt',
+  },
+
+  {
+    id: 4,
+    category: 'education',
+    icon: <FaGraduationCap />,
+    year: '2015',
+    title: 'Engineering Degree <span> Oxford University </span>',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do tempor incididunt ut labore',
+  },
+
+  {
+    id: 5,
+    category: 'education',
+    icon: <FaGraduationCap />,
+    year: '2012',
+    title: 'Master Degree <span> KIEV University </span>',
+    desc: 'Lorem incididunt dolor sit amet, consectetur eiusmod dunt doldunt dol elit, tempor incididunt',
+  },
+
+  {
+    id: 6,
+    category: 'education',
+    icon: <FaGraduationCap />,
+    year: '2009',
+    title: 'Bachelor Degree <span> Tunis High School </span>',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor ut labore',
+  },
+];
+
+export const skills = [
+  {
+    id: 1,
+    title: 'Html5',
+    percentage: '90',
+  },
+
+  {
+    id: 2,
+    title: 'Javascript',
+    percentage: '80',
+  },
+
+  {
+    id: 3,
+    title: 'Css',
+    percentage: '85',
+  },
+
+  {
+    id: 4,
+    title: 'Bootstrap',
+    percentage: '90',
+  },
+
+  {
+    id: 5,
+    title: 'React JS',
+    percentage: '75',
+  },
+
+  {
+    id: 6,
+    title: 'Node JS',
+    percentage: '60',
+  },
+
+  {
+    id: 7,
+    title: 'MongoDB',
+    percentage: '65',
+  },
+
+  {
+    id: 8,
+    title: 'Blockchain',
+    percentage: '45',
+  },
+];
+
+export const portfolio = [
+  {
+    id: 1,
+    img: Work1,
+    title: 'Photo Editing',
+    details: [
+      {
+        icon: <FiFileText />,
+        title: 'Project : ',
+        desc: 'Photo',
+      },
+      {
+        icon: <FiUser />,
+        title: 'Client : ',
+        desc: 'Dribble',
+      },
+      {
+        icon: <FaCode />,
+        title: 'Language : ',
+        desc: 'Adobe Photoshop',
+      },
+      {
+        icon: <FiExternalLink />,
+        title: 'Preview : ',
+        desc: 'www.dribble.com',
+      },
+    ],
+  },
+
+  {
+    id: 2,
+    img: Work2,
+    title: 'Website Design',
+    details: [
+      {
+        icon: <FiFileText />,
+        title: 'Project : ',
+        desc: 'Website',
+      },
+      {
+        icon: <FiUser />,
+        title: 'Client : ',
+        desc: 'Dribble',
+      },
+      {
+        icon: <FaCode />,
+        title: 'Language : ',
+        desc: 'React JS',
+      },
+      {
+        icon: <FiExternalLink />,
+        title: 'Preview : ',
+        desc: 'www.dribble.com',
+      },
+    ],
+  },
+
+  {
+    id: 3,
+    img: Work3,
+    title: 'Video Editing',
+    details: [
+      {
+        icon: <FiFileText />,
+        title: 'Project : ',
+        desc: 'Video',
+      },
+      {
+        icon: <FiUser />,
+        title: 'Client : ',
+        desc: 'Dribble',
+      },
+      {
+        icon: <FaCode />,
+        title: 'Language : ',
+        desc: 'Adobe Premium',
+      },
+      {
+        icon: <FiExternalLink />,
+        title: 'Preview : ',
+        desc: 'www.dribble.com',
+      },
+    ],
+  },
+
+  {
+    id: 4,
+    img: Work4,
+    title: 'Video Editing',
+    details: [
+      {
+        icon: <FiFileText />,
+        title: 'Project : ',
+        desc: 'Video',
+      },
+      {
+        icon: <FiUser />,
+        title: 'Client : ',
+        desc: 'Dribble',
+      },
+      {
+        icon: <FaCode />,
+        title: 'Language : ',
+        desc: 'Adobe Premium',
+      },
+      {
+        icon: <FiExternalLink />,
+        title: 'Preview : ',
+        desc: 'www.dribble.com',
+      },
+    ],
+  },
+
+  {
+    id: 5,
+    img: Work5,
+    title: 'Landing Page',
+    details: [
+      {
+        title: 'Project : ',
+        desc: 'Website',
+      },
+      {
+        title: 'Client : ',
+        desc: 'Dribble',
+      },
+      {
+        title: 'Language : ',
+        desc: 'React JS, Node JS',
+      },
+      {
+        title: 'Preview : ',
+        desc: 'www.dribble.com',
+      },
+    ],
+  },
+
+  {
+    id: 6,
+    img: Work6,
+    title: 'Photo Editing',
+    details: [
+      {
+        icon: <FiFileText />,
+        title: 'Project : ',
+        desc: 'Photo',
+      },
+      {
+        icon: <FiUser />,
+        title: 'Client : ',
+        desc: 'Dribble',
+      },
+      {
+        icon: <FaCode />,
+        title: 'Language : ',
+        desc: 'Adobe Photoshop',
+      },
+      {
+        icon: <FiExternalLink />,
+        title: 'Preview : ',
+        desc: 'www.dibble.com',
+      },
+    ],
+  },
+];
+
+export const themes = [
+  {
+    id: 1,
+    img: Theme1,
+    color: 'hsl(252, 35%, 51%)',
+  },
+
+  {
+    id: 2,
+    img: Theme2,
+    color: 'hsl(4, 93%, 54%)',
+  },
+
+  {
+    id: 3,
+    img: Theme3,
+    color: 'hsl(271, 76%, 53%)',
+  },
+
+  {
+    id: 4,
+    img: Theme4,
+    color: 'hsl(225, 73%, 57%)',
+  },
+
+  {
+    id: 5,
+    img: Theme5,
+    color: 'hsl(43, 74%, 49%)',
+  },
+
+  {
+    id: 6,
+    img: Theme6,
+    color: 'hsl(339, 81%, 66%)',
+  },
+
+  {
+    id: 7,
+    img: Theme7,
+    color: 'hsl(80, 61%, 50%)',
+  },
+
+  {
+    id: 8,
+    img: Theme8,
+    color: 'hsl(19, 96%, 52%)',
+  },
+
+  {
+    id: 9,
+    img: Theme9,
+    color: 'hsl(88, 65%, 43%)',
+  },
+
+  {
+    id: 10,
+    img: Theme10,
+    color: 'hsl(42, 100%, 50%)',
+  },
+];
